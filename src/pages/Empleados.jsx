94090c4159d43f56a6528e21e93bc981ddeb7519
Lucas Fuentes <lucascarpii2002@gmail.com>
2024-09-16 13:33:54 -0300

    Ver cliente modal

--- src/pages/Empleados.jsx
+++ src/pages/Empleados.jsx
@@ -1,23 +1,46 @@
 import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { useEffect, useState } from "react";
-import { Button, Modal, runCode, Table } from "tamnora-react";
+import { AutoForm, Button, Modal, runCode, Table } from "tamnora-react";
 
 
 export function Empleados() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [usersOnDB, setUsersOnDB] = useState([]);
+  const [formData, setFormData] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [alertData, setAlertData] = useState({});
+  const [showAlert, setShowAlert] = useState(false);
+  const [idSelected, setIdSelected] = useState(null);
 
+  const getData = async () => {
+    await runCode('-st usuarios WHERE tipo_usuario = "empleado"').then(res => {
+      setTableData(res)
+    })
+  };
 
   useEffect(() => {
-    runCode('-st usuarios WHERE tipo_usuario = "empleado"').then(res => setUsersOnDB(res))
-  }, [])
+    getData();
+  }, [isModalOpen]);
+
+  useEffect(() => { window.scrollTo(0, 0) }, []);
+
   function closeModal() {
     setIsModalOpen(false)
   }
+  function updateData() {
+    console.log('hola')
+  }
+
+  const verCliente = async (rowData) => {
+    setFormData(null);
+    const res = await runCode(`-st usuarios -wr id = ${rowData.id}`);
+    setFormData(res[0]);
+    setIdSelected(res[0].cli_id)
+    setIsModalOpen(true)
+  };
 
   return (
     <>
-      <section className="flex flex-col">
+      <section className="flex flex-col mb-20">
         <div className="flex justify-between items-start mb-6">
           <div className="flex items-center gap-2">
             <button className="p-2 hover:bg-zinc-200 rounded-full"><ArrowLeftIcon className="size-4" /></button>
@@ -27,15 +50,27 @@ export function Empleados() {
             Nuevo
           </Button>
         </div>
-        <Table data={usersOnDB} />
+        <Table data={tableData} onRowClick={verCliente} />
       </section>
-
-      <Modal
-        isOpen={isModalOpen}
-        handleModal={closeModal}
-        size="3xl">
-
-      </Modal>
+      {
+        formData &&
+        <Modal
+          isOpen={isModalOpen}
+          handleModal={closeModal}
+          size="3xl"
+        >
+          <AutoForm
+            data={formData}
+            onSubmit={updateData}
+            primaryKey="id"
+            table="usuarios"
+            isHidden={['id']}
+            colsWidth={{ nombre_usuario: 'col-span-12 sm:col-span-4', contraseña: 'col-span-12 sm:col-span-4', tipo_usuario: 'col-span-12 sm:col-span-4' }}
+            names={{ nombre_usuario: 'Nombre y apellido', contraseña: 'Contraseña', tipo_usuario: 'Tipo de usuario' }}
+            types={{ tipo_usuario: { type: 'select', options: [{ value: 'empleado', label: 'Empleado' }, { value: 'administrador', label: 'Administrador' }] } }}
+          />
+        </Modal>
+      }
     </>
   )
 }
